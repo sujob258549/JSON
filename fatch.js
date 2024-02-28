a0ff056a2199e0185f8ab3dfe95532813ce77f25
DevSujon <sujonmaia274@gmail.com>
2024-02-28 06:24:33 +0600

    jason commmit

--- fatch.js
+++ fatch.js
@@ -8,5 +8,46 @@
 function fast2(){
 fetch('https://jsonplaceholder.typicode.com/todos')
 .then(res => res.json())
-.then(display => console.log(display))
+.then(display => coll(display))
 }
+
+function coll(display){
+    for(const dis of display){
+       const ul = document.getElementById('list-item');
+       const li = document.createElement('li');
+      console.log( li.innerText = dis.title);
+       ul.appendChild(li);
+    }
+}
+
+
+function divContainer(){
+    fetch('https://jsonplaceholder.typicode.com/photos')
+    .then(res => res.json())
+    .then(displays => mainplay(displays))
+}
+
+function mainplay(plays){
+
+    
+    for(const play of plays){
+        // console.log(play);
+        const divContainers = document.getElementById('div-container');
+        const createDivContainer = document.createElement('div');
+        createDivContainer.classList.add("plas");
+        createDivContainer.innerHTML = `
+        <h3>${play.id}</h3>
+        <h4>${play.title}</h4>
+        <p>${play.thumbnailUrl}</p>
+        
+        `;
+
+        divContainers.appendChild(createDivContainer);
+
+
+    }
+}
+
+
+
+divContainer();
